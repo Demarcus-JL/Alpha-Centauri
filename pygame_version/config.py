acd9e54cd3531b768b1a1c9af162812d7d5fac34
VSparl <sparleanu.victor@gmail.com>
2025-01-06 11:52:52 +0100

    added config file to control constants used throughout the simulation

---
+++ pygame_version/config.py
@@ -0,0 +1,12 @@
+"""Module to store configuration variables for the simulation."""
+
+# Constants
+FPS: int = 60
+# Simulated seconds that pass between simulation steps
+TIMESTEP: float = 1e7  # ca. 3.8 months on earth
+START_SCREEN_SIZE: tuple[int, int] = (400, 400)  # Native screen resolution
+
+# Newton's gravitational constant
+G = 6.67430e-11  # in N m^2 kg^-2
+# Scale factor so you can actually see the objects
+SCALE_FACTOR: int = 500
